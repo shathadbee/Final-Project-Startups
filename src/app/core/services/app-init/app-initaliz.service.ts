3c7b5a8457f4d0ccd10d30e86729307d09df1e47
shathadbe <shathaaldbee@gmail.com>
2023-01-17 23:23:08 +0200

    project

--- src/app/core/services/app-init/app-initaliz.service.ts
+++ src/app/core/services/app-init/app-initaliz.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppInitalizService {
-  constructor(private _authService: AuthService) {}
+  constructor(private _authService: AuthService,
+    private fireAuth:AngularFireAuth) {}
 
   appInitializer(): Promise<any> {
     return new Promise<boolean>((resolve) => {
-      //this._authService.getUserById('') ;
+      this._authService.getUserById('') ;
       resolve(true);
     });
   }
 }
+
 export function appInitializer(config: AppInitalizService) {
   return () => config.appInitializer();
 }
